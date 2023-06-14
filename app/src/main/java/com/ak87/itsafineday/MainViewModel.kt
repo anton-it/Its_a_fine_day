36edaf7b3017a8bd2da8afeae40febb1bd50457d
Admin1 <a89086829981@gmail.com>
2023-06-14 16:28:19 +0300

    Add - permissions - dependencies - add MainViewModel

---
+++ app/src/main/java/com/ak87/itsafineday/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.ak87.itsafineday
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
