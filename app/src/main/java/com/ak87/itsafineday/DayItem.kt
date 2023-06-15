07ccdca50a7063c5e153629b5c91f0b8742d8902
Admin1 <a89086829981@gmail.com>
2023-06-15 13:38:26 +0300

    Add DayItem.kt

---
+++ app/src/main/java/com/ak87/itsafineday/DayItem.kt
@@ -0,0 +1,12 @@
+package com.ak87.itsafineday
+
+data class DayItem(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val hours: String
+)
